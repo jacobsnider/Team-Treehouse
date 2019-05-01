70504f140dc5ed5d83d58aa033ca7bcb578b3fd1
Jake-thegangle-Snider <jakesniderlmt@gmail.com>
2019-05-01 13:18:14 -0400

    Practice 01

---
+++  Practice Basic Variables, Input, and Output /Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Treehouse.PracticeSession
+{
+    class Program
+    {
+        static void Main()
+        {
+            // TODO Declare and assign a "thing" string variable.
+            // Example: "movie", "book", "color", etc.
+            
+            // TODO Prompt the user with the text "What is your name?" 
+            // and assign their value to a "name" string variable.
+            
+            // TODO Prompt the user with the text 
+            // "What is your favorite <thing>?" 
+            // and assign their value to a "favoriteThing" string variable.
+            
+            // TODO Write the user's name and favorite thing to the console.
+            // Example: "My name is James and my favorite movie is Toy Story."
+        }
+    }
+}
