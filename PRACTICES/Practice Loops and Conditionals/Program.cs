ab6d5bb3401a56ae38aaf5f8f85d92af6c75ad79
Jake-thegangle-Snider <jakesniderlmt@gmail.com>
2019-05-01 17:38:54 -0400

    Loops and Conditionals Complete

---
+++ PRACTICES/Practice Loops and Conditionals/Program.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Treehouse.PracticeSession
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // TODO Declare a boolean variable named "keepGoing"
+            // and assign it a value of "true". Use this variable to 
+            // know when to exit the while loop.
+
+            // TODO Define a while loop.
+            // Keep looping as long as the variable "keepGoing" has a value of "true".
+
+                // TODO Prompt the user with the text "Enter a number:" 
+                // and assign their value to an "entry" string variable.
+                
+                // TODO If the user entered the text "quit"...
+
+                    // TODO Exit the program.
+
+                // TODO Else the user didn't enter the text "quit"...
+
+                    // TODO Parse the user's entry to an integer
+                    
+                    // TODO Square the user's provided number 
+                    // (i.e. multiply the number by itself).
+                    
+                    // TODO Output the result.
+                    // Example: "The square of 2 is 4." or 
+                    // "2 multiplied by itself is equal to 4."
+            
+            // TODO Output the text "Goodbye!" after exiting the loop.
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
