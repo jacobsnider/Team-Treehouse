8b71b84b4bf95aae68301d7f20fe792db8fd4463
Jake-thegangle-Snider <jakesniderlmt@gmail.com>
2019-05-13 11:32:05 -0400

    Calculate a Statistic

--- SoccerStats/SoccerStats/GameResult.cs
+++ SoccerStats/SoccerStats/GameResult.cs
@@ -15,6 +15,13 @@ namespace SoccerStats
         public int ShotsOnGoal { get; set; }
         public int ShotsOffGoal { get; set; }
         public double PossessionPercent { get; set; }
+        public double ConversionRate
+        {
+            get
+            {
+                return (double)Goals / (double)GoalAttempts;
+            }
+        } 
     }
 
     public enum HomeOrAway
--- SoccerStats/SoccerStats/Program.cs
+++ SoccerStats/SoccerStats/Program.cs
@@ -72,6 +72,8 @@ namespace SoccerStats
                     {
                         gameResult.PossessionPercent = possessionPercent;
                     }
+
+                    soccerResults.Add(gameResult);
                 }
             }
             return soccerResults;
